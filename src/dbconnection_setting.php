14a174f23d05bc00ceffab049a2d430b54da179e
ag5085 <ag5085@nyu.edu>
2017-08-13 01:41:22 -0400

    codebase

---
+++ src/dbconnection_setting.php
@@ -0,0 +1,11 @@
+<?php
+	$host = "pdc-amd01.poly.edu";
+	//$host = "localhost";
+	$user = "ag5085";
+	//$pass = "******";
+	//$db = "ag5085";
+	//$user = "postgres";
+	$db = "postgres";
+	$pass = "******";
+	$port = "5432";
+?>
